2d7591f7454b17ab440ce4cf676381f31f748afc
Denis Robert <denis.r2901@gmail.com>
2024-08-05 12:08:18 +0530

    Update write_table.py

--- backend/write_table.py
+++ backend/write_table.py
@@ -85,7 +85,90 @@ def write_deal(data):
         # conn.close()
     return deal_id
 
-
+def update_deal(data):
+    software,hardware,license,customization,enhancement,deployment,support,professional_service = [v for _, v in data.get('workNature', {}).items()]
+    deal_id=data.get('deal_id')
+    customer_name=data.get('customer_name', '')
+    customer_type=data.get('customer_type', '')
+    business_type=data.get('business_type', '')
+    guarantee=data.get('guarantee', '')
+    opportunity=data.get('opportunity', '')
+    bidding=data.get('bidding', '')
+    start_date=data.get('start_date', '')
+    end_date=data.get('end_date', '')
+    total_contract=data.get('total_contract', '')
+    region=data.get('region', '')
+    domain=data.get('domain', '')
+    direct_sale=data.get('direct_sale', '')
+    partner=data.get('partner', '')
+    third_party=data.get('third_party', '')
+    penalty=data.get('penalty', '')
+    acc_manager=data.get('acc_manager', '')
+    presales_spoc=data.get('presales_spoc', '')
+    currency=data.get('currency', '')
+    cust_budget=data.get('cust_budget', '')
+    travel_expense=data.get('travel_expense', '')
+    tool_license=data.get('tool_license', '')
+    shift_allowance=data.get('shift_allowance', '')
+    weekend_allowance=data.get('weekend_allowance', '')
+    on_call_allowance=data.get('on_call_allowance', '')
+    one_time_cost=data.get('one_time_cost', '')
+    vat=data.get('vat', '')
+    misc=data.get('misc', '')
+    try:
+        cur.execute(
+    '''
+    update kif set customer_name=%s, customer_type=%s, business_type=%s, guarantee=%s, opportunity=%s,
+    software=%s, hardware=%s, license=%s, customization=%s, enhancement=%s, deployment=%s, support=%s,
+    professional_service=%s, bidding=%s, start_date=%s, end_date=%s, total_contract=%s, region=%s, domain=%s,
+    direct_sale=%s, partner=%s, third_party=%s, penalty=%s, acc_manager=%s, presales_spoc=%s, currency=%s,
+    cust_budget=%s, travel_expense=%s, tool_license=%s, shift_allowance=%s, weekend_allowance=%s,
+    on_call_allowance=%s, one_time_cost=%s, vat=%s, misc=%s) where deal_id=%s;''',
+    (
+        customer_name,
+        customer_type,
+        business_type,
+        guarantee,
+        opportunity,
+        software,
+        hardware,
+        license,
+        customization,
+        enhancement,
+        deployment,
+        support,
+        professional_service,
+        bidding,
+        start_date,
+        end_date,
+        total_contract,
+        region,
+        domain,
+        direct_sale,
+        partner,
+        third_party,
+        penalty,
+        acc_manager,
+        presales_spoc,
+        currency,
+        cust_budget,
+        travel_expense,
+        tool_license,
+        shift_allowance,
+        weekend_allowance,
+        on_call_allowance,
+        one_time_cost,
+        vat,
+        misc,
+        deal_id
+    )
+        )
+    except Exception as e:
+        conn.rollback()
+        print(f"An error occurred: {e}")
+        raise
+    finally:
+        conn.commit()
 
 def del_deal(data):
     deal_id=data.get('deal_id', '')
